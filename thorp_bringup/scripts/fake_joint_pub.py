10a4f7f03d1fc9c8a358c47d238634221b32a12f
corot <jsantossimon@gmail.com>
2014-05-20 20:18:31 +0200

    Publish joint states for motor wheels so they are properly placed

---
+++ thorp_bringup/scripts/fake_joint_pub.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest("thorp_bringup")
+import rospy
+from sensor_msgs.msg import JointState
+
+rospy.init_node("fake_joint_pub")
+p = rospy.Publisher('joint_states', JointState)
+
+msg = JointState()
+msg.name = ["wheel_left_joint", "wheel_right_joint", "caster_front_joint", "caster_rear_joint"]
+msg.position = [0.0 for name in msg.name]
+msg.velocity = [0.0 for name in msg.name]
+
+while not rospy.is_shutdown():
+    msg.header.stamp = rospy.Time.now()
+    p.publish(msg)
+    rospy.sleep(0.1)
+
