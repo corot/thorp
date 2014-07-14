6fac3804338a3ea72f4c24366497bc522a1325ad
Jorge Santos <jsantos@ist.tugraz.at>
2014-07-14 11:51:03 +0200

    Add queue_size=5 parameter to all publishers to avoid the WARN on Indigo

--- thorp_bringup/scripts/fake_joint_pub.py
+++ thorp_bringup/scripts/fake_joint_pub.py
@@ -5,7 +5,7 @@ import rospy
 from sensor_msgs.msg import JointState
 
 rospy.init_node("fake_joint_pub")
-p = rospy.Publisher('joint_states', JointState)
+p = rospy.Publisher('joint_states', JointState, queue_size=5)
 
 msg = JointState()
 msg.name = ["wheel_left_joint", "wheel_right_joint", "gripper_link_joint"]
