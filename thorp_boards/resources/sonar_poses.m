4f9aa649066e290adb0aa8f453c470d05d3809f1
corot <jsantossimon@gmail.com>
2014-12-05 21:08:26 +0100

    Add sonar mount mesh

--- thorp_boards/resources/sonar_poses.m
+++ thorp_boards/resources/sonar_poses.m
@@ -1,6 +1,6 @@
-for a = -pi/2:pi/10:pi/2
-    x = cos(a)*0.155;
-    y = sin(a)*0.155;
+﻿for a = -pi/2:pi/10:pi/2
+    x = cos(a)*0.138;
+    y = sin(a)*0.138;
     z = 0.09;
     disp([x, y, z, a, 0.0, 0.0])
 end
