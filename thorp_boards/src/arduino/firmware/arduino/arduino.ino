67136a6041ed8368488560b4146388de01f82b83
corot <jsantossimon@gmail.com>
2015-07-03 00:43:38 +0200

    Use port registers to trigger all sonars at the same time

--- thorp_boards/src/arduino/firmware/arduino/arduino.ino
+++ thorp_boards/src/arduino/firmware/arduino/arduino.ino
@@ -7,17 +7,36 @@
 int IR_COUNT = 4;
 int HS_COUNT = 11;
 int inputPin[] = {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 15, 14, 13, 12 };
-int triggPin[] = { 22, 28, 32, 36, 40, 44, 48, 39, 43, 47, 51 };
+int triggPin[] = { 48, 44, 40, 36, 32, 28, 22, 35, 47, 43, 39 };
 byte outBuffer[sizeof(inputPin) + 2];
+
+
+void readAnalog(int index)
+{
+  // read and pack the obtained value as two bytes,
+  // always skipping the first two "protocol" bytes
+  int ar = analogRead(A0 + inputPin[index]);
+  outBuffer[(index+1)*2+1] = (byte)((ar >> 8) & 0xFF);
+  outBuffer[(index+1)*2]   = (byte)(ar & 0xFF) ;
+}
 
 
 void setup() {
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
 
-  // initialize all trigger PINs
-  for (int i = 0; i < HS_COUNT; i++)
-    pinMode(triggPin[i], OUTPUT);
+  // we use port registers to trigger all sonars at the same time
+  // compose the masks (see link below) with the PIN indexes and initialize at LOW
+  // http://stackoverflow.com/questions/21417521/arduino-use-all-ports-atmega2560
+  DDRA = B01000001;
+  DDRC = B00100110;
+  DDRG = B00000110;
+  DDRL = B01100110;
+  
+  PORTA = DDRA & 0x00;
+  PORTC = DDRC & 0x00;
+  PORTG = DDRG & 0x00;
+  PORTL = DDRL & 0x00;
 
   // set the first two bytes to signal the beginning of a readings set; note that our
   // protocol is really crude... we should avoid writing faster than the PC reads!
@@ -27,23 +46,27 @@ void setup() {
 
 void loop() {
   // trigger sonars reading
-  for (int i = 0; i < HS_COUNT; i++)
-    digitalWrite(triggPin[i], HIGH);
+  PORTA = DDRA & 0xFF;
+  PORTC = DDRC & 0xFF;
+  PORTG = DDRG & 0xFF;
+  PORTL = DDRL & 0xFF;
+  
   delayMicroseconds(20);
-  for (int i = 0; i < HS_COUNT; i++)
-    digitalWrite(triggPin[i], LOW);
 
+  PORTA = DDRA & 0x00;
+  PORTC = DDRC & 0x00;
+  PORTG = DDRG & 0x00;
+  PORTL = DDRL & 0x00;
+  
   // delay a bit less than the sensors period, to compensate the wasted time on reading
-  delay(50);
+  delay(46);
 
-  // read all analog input PINs and pack the obtained value as two bytes, skipping the
-  // first two "protocol" bytes
-  for (int i = 0; i < HS_COUNT + IR_COUNT; i++) {
-    int ar = analogRead(A0 + inputPin[i]);
-    outBuffer[(i+1)*2+1] = (byte)((ar >> 8) & 0xFF);
-    outBuffer[(i+1)*2]   = (byte)(ar & 0xFF) ;
-  }
+  // read all analog input PINs, both sonars and IR sensors that need not triggering
+  for (int i = 0; i < HS_COUNT + IR_COUNT; i++)
+    readAnalog(i);
+
   // write buffer to serial port
   Serial.write(outBuffer, sizeof(outBuffer));
   Serial.flush();
 }
+
