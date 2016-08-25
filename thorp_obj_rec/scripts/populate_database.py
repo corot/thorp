9f5b9c173e79f253369cb24527faebe328ba53c1
corot <jsantossimon@gmail.com>
2016-08-26 00:15:16 +0200

    Call OpenSCAD to generate the mesh if it doesn't exist

--- thorp_obj_rec/scripts/populate_database.py
+++ thorp_obj_rec/scripts/populate_database.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
-# Populate Thorp objects database with a bunch of simple objects
-# WARN: this script assumes that the database doesn't contain any of this objects; if so, the script will hung while prompting for overwrite 
+# Populate Thorp objects database with a bunch of simple objects.  WARN: this script assumes that the
+# database doesn't contain any of this objects; if so, the script will hung while prompting for overwrite 
 
 import re
 import rospkg
 import subprocess
+import os.path
 
 try:
     # use rospkg to get the absolute path of meshes on thorp_obj_rec
     rospack = rospkg.RosPack()
 
-    # For each object, call object_add, get the generated object id and call mesh_add
-    # The expected output of object_add is:   Stored new object with id: 9628d89172994443d3f7a535ca000abd  WARN: including a \n!
+    # For each object, check if we already have a STL mesh. If not, generate it with OpenSCAD. We must convert to
+    # binary, as OpenSCAD can only export ASCII STL by now (see https://github.com/openscad/openscad/issues/1555)
+    # For already binary meshes, conversion just does nothing.
+    # Once we have our mesh, we call object_recognition_core object_add, get the generated object id and call mesh_add
+    # Expected output of object_add: Stored new object with id: 9628d89172994443d3f7a535ca000abd  WARN: including a \n!
 
     objects = [['coke_can', 'A universal can of coke', 'coke.stl'],
                ['lipstick', 'Uriage lipstick, 7.4cm long', 'lipstick.stl'],
@@ -20,11 +24,16 @@ try:
                ['cube 2.5cm', 'Untextured 2.5 cm side cube', 'cube_2_5.stl']]
 
     for object in objects:
-        print subprocess.check_output(['admesh', '-b', 'tmp.stl', rospack.get_path('thorp_obj_rec') + "/meshes/" + object[2]])
+        if os.path.exists(rospack.get_path('thorp_obj_rec') + "/meshes/" + object[2]):
+            print subprocess.check_output(['cp', rospack.get_path('thorp_obj_rec') + "/meshes/" + object[2], 'tmp.ascii.stl'])
+        else:
+            print subprocess.check_output(['openscad', '-DOBJ="' + object[2][:-4] + '"', '-o', 'tmp.ascii.stl', 'gen_meshes.scad'])
+        print subprocess.check_output(['admesh', '-b', 'tmp.bin.stl', 'tmp.ascii.stl'])
         out = subprocess.check_output(['rosrun', 'object_recognition_core', 'object_add.py', '-n', object[0], '-d', object[1], '--commit'])
         id = re.split(' ', out)[-1][:-1]
-        print subprocess.check_output(['rosrun', 'object_recognition_core', 'mesh_add.py', id, 'tmp.stl', '--commit'])
-        print subprocess.check_output(['rm', 'tmp.stl'])
+        print out
+        print subprocess.check_output(['rosrun', 'object_recognition_core', 'mesh_add.py', id, 'tmp.bin.stl', '--commit'])
+        print subprocess.check_output(['rm', 'tmp.ascii.stl', 'tmp.bin.stl'])
 except rospkg.common.ResourceNotFound as err:
     print "get package path failed: " + str(err)
 except subprocess.CalledProcessError as err:
