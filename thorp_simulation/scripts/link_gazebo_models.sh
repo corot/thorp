d1bcc2ba9bcef8fa75d21ce86a3261022b6b09e8
corot <jsantossimon@gmail.com>
2020-05-09 21:38:30 +0900

    Ensure Gazebo can load our models (so no links needed)

--- thorp_simulation/scripts/link_gazebo_models.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd
-cd .gazebo/models
-ln -s ~/catkin_ws/thorp/src/thorp/thorp_simulation/worlds/gazebo/models/* .
-mkdir doll_table
-cd doll_table
-ln -s ~/catkin_ws/perception/src/rail_collada_models/meshes/original_models/doll_furniture .
