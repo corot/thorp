98a164e95fb59d3ad6ce2f25a2b674a40b77f3c7
corot <jsantossimon@gmail.com>
2020-05-10 12:38:53 +0900

    Cleanup cannon plugin and rename particle_sphere as rocket

--- thorp_cannon/src/gazebo_cannon_plugin.cpp
+++ thorp_cannon/src/gazebo_cannon_plugin.cpp
@@ -19,10 +19,10 @@
 namespace gazebo
 {
 
-class ParticleShooterPlugin : public ModelPlugin
+class ThorpCannonPlugin : public ModelPlugin
 {
 public:
-  ParticleShooterPlugin() : ModelPlugin()
+  ThorpCannonPlugin() : ModelPlugin()
   {
   }
 
@@ -37,12 +37,11 @@ public:
     }
 
     this->model = _model;
-    GZ_ASSERT(this->model != NULL, "Got NULL model pointer!");
+    GZ_ASSERT(this->model != nullptr, "Got nullptr model pointer!");
+    this->world = _model->GetWorld();
     this->sdf = _sdf;
-    GZ_ASSERT(this->sdf != NULL, "Got NULL SDF element pointer!");
+    GZ_ASSERT(this->sdf != nullptr, "Got nullptr SDF element pointer!");
 
-    if (_sdf->HasElement("origin_link"))
-      this->origin_link = _sdf->Get<std::string>("origin_link");
     if (_sdf->HasElement("axis_of_fire"))
       this->axis_of_fire = _sdf->Get<std::string>("axis_of_fire");
     if (_sdf->HasElement("rate_of_fire"))
@@ -51,6 +50,11 @@ public:
       this->shoot_force = _sdf->Get<double>("shoot_force");
     if (_sdf->HasElement("rocket_count"))
       this->rocket_count = _sdf->Get<double>("rocket_count");
+
+    auto cannon_link_name = _sdf->HasElement("cannon_link") ? _sdf->Get<std::string>("cannon_link") : "cannon_link";
+    ROS_DEBUG("Get link %s for model %s", cannon_link_name.c_str(), this->model->GetName().c_str());
+    this->cannon_link = this->model->GetLink(cannon_link_name);
+    GZ_ASSERT(this->cannon_link !=nullptr, "Got nullptr link pointer!");
 
     if (this->axis_of_fire == "x")
       this->direction_of_fire = ignition::math::Vector3d(this->shoot_force, 0, 0);
@@ -61,18 +65,17 @@ public:
 
 
     this->trigger_sub = nh.subscribe<arbotix_msgs::Digital>("arbotix/cannon_trigger", 5,
-                                                            &ParticleShooterPlugin::triggerCB, this);
+                                                            &ThorpCannonPlugin::TriggerCB, this);
 
-    this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-        std::bind(&ParticleShooterPlugin::OnUpdate, this));
+    this->update_connection = event::Events::ConnectWorldUpdateBegin(std::bind(&ThorpCannonPlugin::OnUpdate, this));
 
-    ROS_DEBUG("Particle Shooter Ready....");
+    ROS_DEBUG("Thorp cannon gazebo plugin ready....");
   }
 
-  void triggerCB(const arbotix_msgs::Digital::ConstPtr& msg)
+  void TriggerCB(const arbotix_msgs::Digital::ConstPtr& msg)
   {
     this->firing = msg->value > 0;
-    ROS_ERROR("Trigger! %s", this->firing ? "ON" : "OFF");
+    ROS_DEBUG("Trigger! %s", this->firing ? "ON" : "OFF");
   }
 
   void Reset()
@@ -87,88 +90,79 @@ public:
     this->reseting_plugin = false;
   }
 
-
   // Called by the world update start event
   public: void OnUpdate()
   {
     if (this->reseting_plugin)
     {
-      ROS_ERROR("Reseting in Process, please wait...");
+      ROS_ERROR("Reseting in process, please wait...");
       return;
     }
 
     if (this->firing && this->next_rocket <= this->rocket_count)
     {
       // Trigger pressed and still have rockets; fire if we meet our rate of fire
-      double new_secs = this->model->GetWorld()->SimTime().Float();
+      double new_secs = this->world->SimTime().Float();
       double delta = new_secs - this->last_shot_time;
       if (delta >= 1.0/this->rate_of_fire)
       {
         FireRocket(this->next_rocket++);
-//        ++this->next_rocket;
         this->last_shot_time = new_secs;
       }
     }
   }
 
-
   void WaitForseconds(float seconds_to_wait)
   {
     unsigned int microseconds;
     microseconds = seconds_to_wait * 1e6;
-    ROS_WARN("Waiting for %f seconds",seconds_to_wait);
+    ROS_DEBUG("Waiting for %f seconds",seconds_to_wait);
     usleep(microseconds);
-    ROS_WARN("Done waiting...");
-
+    ROS_DEBUG("Done waiting...");
   }
-
 
   void FireRocket(int number)
   {
-    std::string model_name = this->particle_base_name + std::to_string(number);
-    ROS_WARN("Load rocket %d model: %s", number, model_name.c_str());
-    auto rocket_model = this->model->GetWorld()->ModelByName(model_name);
-    ROS_WARN("Model Name=%d", rocket_model.get());
+    std::string model_name = this->rocket_models_base_name + std::to_string(number);
+    ROS_DEBUG("Loading rocket %d model: %s", number, model_name.c_str());
+    auto rocket_model = this->world->ModelByName(model_name);
+    GZ_ASSERT(rocket_model, "Got nullptr rocket model pointer!");
 
-    ROS_WARN("origin model=%s  link=%s", this->model->GetName().c_str(),this->origin_link.c_str());
-    auto po_link = this->model->GetLink(this->origin_link);
-    ROS_WARN("origin_model model=%s", po_link->GetName().c_str());
-    auto po_pose = po_link->WorldPose();
+    auto origin_pose = this->cannon_link->WorldPose();
 
-    this->SetRocketPose(rocket_model, po_pose);
-    this->SetRocketForce(rocket_model, po_pose);
+    this->SetRocketPose(*rocket_model, origin_pose);
+    this->SetRocketForce(*rocket_model, origin_pose);
   }
 
-
-  void SetRocketPose(boost::shared_ptr<gazebo::physics::Model> rocket_model, const ignition::math::Pose3d &cannon_pose)
+  void SetRocketPose(gazebo::physics::Model &rocket_model, const ignition::math::Pose3d &cannon_pose)
   {
     ignition::math::Pose3d rocket_pose = ignition::math::Pose3d(0.1, 0.0, 0.0, 0.0, 0.0, 0.0) * cannon_pose;
-    ROS_WARN("POSE [X, Y, Z, Roll, Pitch, Yaw] [%f, %f, %f, %f, %f, %f], model=%s",
+    ROS_DEBUG("POSE [X, Y, Z, Roll, Pitch, Yaw] = [%f, %f, %f, %f, %f, %f]",
       rocket_pose.Pos().X(), rocket_pose.Pos().Y(), rocket_pose.Pos().Z(),
-      rocket_pose.Rot().Euler().X(), rocket_pose.Rot().Euler().Y(), rocket_pose.Rot().Euler().Z(),
-             rocket_model->GetName().c_str());
-    rocket_model->SetWorldPose(rocket_pose);
+      rocket_pose.Rot().Euler().X(), rocket_pose.Rot().Euler().Y(), rocket_pose.Rot().Euler().Z());
+    rocket_model.SetWorldPose(rocket_pose);
   }
-  
-  
-  void SetRocketForce(boost::shared_ptr<gazebo::physics::Model> rocket_model, const ignition::math::Pose3d &cannon_pose)
+
+  void SetRocketForce(gazebo::physics::Model &rocket_model, const ignition::math::Pose3d &cannon_pose)
   {
     auto force_vector = cannon_pose.Rot() * this->direction_of_fire;
 
-    ROS_WARN("FORCE APPLIED[X,Y,Z]=[%f,%f,%f]", force_vector.X(), force_vector.Y(), force_vector.Z());
-    rocket_model->GetLink("link")->SetForce(force_vector);
+    ROS_DEBUG("FORCE APPLIED [X, Y, Z] = [%f, %f, %f]", force_vector.X(), force_vector.Y(), force_vector.Z());
+    rocket_model.GetLink("link")->SetForce(force_vector);
   }
 
 
   // Pointer to the update event connection
-  private: event::ConnectionPtr updateConnection;
+  private: event::ConnectionPtr update_connection;
   
   /// \brief World pointer.
+  protected: gazebo::physics::WorldPtr world;
+  /// \brief Model pointer.
   protected: gazebo::physics::ModelPtr model;
+  /// \brief Link pointer.
+  protected: gazebo::physics::LinkPtr cannon_link;
   /// \brief SDF pointer.
   protected: sdf::ElementPtr sdf;
-  /// \brief Maps model IDs to ModelNames
-  private: std::map<int, std::string> modelIDToName;
 
   // ROS interface
   ros::NodeHandle nh;
@@ -183,14 +177,12 @@ public:
   double last_shot_time = -1.0;
   ignition::math::Vector3d direction_of_fire;
   std::string axis_of_fire = "x";
-  std::string origin_link = "cannon_link";
 
   // Reseting Flag
   bool reseting_plugin = false;
   
-  std::string particle_base_name = "particle_sphere";
-
+  std::string rocket_models_base_name = "rocket";
 };
 
-GZ_REGISTER_MODEL_PLUGIN(ParticleShooterPlugin)
+GZ_REGISTER_MODEL_PLUGIN(ThorpCannonPlugin)
 }
