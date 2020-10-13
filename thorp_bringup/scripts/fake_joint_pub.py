15576edbbe669b47be05d08f1bac9c3d41797f78
corot <jorge.santos@rapyuta-robotics.com>
2020-10-14 00:10:54 +0900

    Publish fake joints at 5 Hz but with time in the future to extend its validity

--- thorp_bringup/scripts/fake_joint_pub.py
+++ thorp_bringup/scripts/fake_joint_pub.py
@@ -8,12 +8,12 @@ p = rospy.Publisher('joint_states', JointState, queue_size=5)
 
 msg = JointState()
 msg.name = ["wheel_left_joint", "wheel_right_joint"]
-msg.position = [0.0 for name in msg.name]
-msg.velocity = [0.0 for name in msg.name]
+msg.position = [0.0] * len(msg.name)
+msg.velocity = [0.0] * len(msg.name)
 
-rate = rospy.Rate(50.0)
+rate = rospy.Rate(5.0)
 while not rospy.is_shutdown():
-    msg.header.stamp = rospy.Time.now()
+    msg.header.stamp = rospy.Time.now() + rospy.Duration(0.5)  # make the state valid into the future to allow slow rate
     p.publish(msg)
     try:
         rate.sleep()
