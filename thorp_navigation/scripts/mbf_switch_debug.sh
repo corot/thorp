dfbdd9e9de68ee01241b5792e1df05f93c786c40
corot <jsantossimon@gmail.com>
2024-01-19 22:26:46 +0900

    Move show_velocity.py to thorp_navigation

--- thorp_navigation/scripts/mbf_switch_debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/jorge/catkin_ws/thorp/src/thorp/thorp_navigation/launch/includes/
-cp move_base_flex.launch.xml move_base_flex.launch.xml.tmp
-cp move_base_flex.launch.xml.debug move_base_flex.launch.xml
-cp move_base_flex.launch.xml.tmp move_base_flex.launch.xml.debug
-rm move_base_flex.launch.xml.tmp
